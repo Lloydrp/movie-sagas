713a73cf4b13db64dd478c7187771aa3bb8845cb
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-07 17:49:31 -0500

    setup click handlers for add and local state

--- src/components/Pages/AddMovie/AddMovie.jsx
+++ src/components/Pages/AddMovie/AddMovie.jsx
@@ -1,22 +1,50 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 function AddMovie() {
   //Setup router variables
   const history = useHistory();
+  //Setup redux variables
+  const dispatch = useDispatch();
+  //Setup local state for inputs
+  const [title, setTitle] = useState("");
+  const [poster, setPoster] = useState("");
+  const [description, setDescription] = useState("");
+
+  //Begin function to handle submit clicks
+  function clickHandler() {
+    // dispatch({ type: "ADD_MOVIE", payload: { title, poster, description } });
+    setTitle("");
+    setPoster("");
+    setDescription("");
+  } //end clickHandler
 
   return (
     <section>
       <h2>Add Movie</h2>
-      <input type="text" placeholder="Enter movie title..." />
-      <input type="text" placeholder="Enter movie poster url..." />
+      <input
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        type="text"
+        placeholder="Enter movie title..."
+      />
+      <input
+        onChange={(event) => setPoster(event.target.value)}
+        value={poster}
+        type="text"
+        placeholder="Enter movie poster url..."
+      />
       <textarea
+        onChange={(event) => setDescription(event.target.value)}
+        value={description}
         name="movie-description"
         id="movie-description"
         cols="30"
         rows="10"
         placeholder="Enter movie description..."
       ></textarea>
-      <button>Add Movie</button>
+      <button onClick={() => clickHandler()}>Add Movie</button>
       <button onClick={() => history.replace("/")}>Cancel</button>
     </section>
   );
