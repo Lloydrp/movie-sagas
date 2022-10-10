07938f976156e7c26de26d5a7da013bc266eb98f
Lloyd Prock <lloyd.prock@gmail.com>
2022-10-09 22:55:50 -0500

    header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,22 @@
 import { Route, useHistory } from "react-router-dom";
 
-function Header({ addButton }) {
+function Header() {
   //Setup router variables
   const history = useHistory();
   return (
-    <header>
-      <h1 className="bg-slate-600">The Movie Saga!</h1>
+    <header className="bg-blue-700 text-white">
+      <h1 className="text-2xl font-bold">ProlloTech Movie Management</h1>
       <Route
         exact
         path="/"
         render={() => (
           <div>
-            <button onClick={() => history.push("/addmovie")}>Add Movie</button>
+            <button
+              className="mt-5 inline-flex items-center rounded-full border-2 border-white bg-green-500 px-2 hover:border-black hover:bg-white hover:text-black"
+              onClick={() => history.push("/addmovie")}
+            >
+              +
+            </button>
           </div>
         )}
       />
